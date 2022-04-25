96ca20a6f4cb4bc5a44cea2d39b87647dd606bf7
Pablo Peña <pablopenia.dev@gmail.com>
2022-04-25 14:07:42 -0300

    1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,7 +4,6 @@ function App() {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const handler = setter => setter(prev => prev + 1)
-  const all = good + neutral + bad
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -13,20 +12,27 @@ function App() {
         <button onClick={() => handler(setNeutral)}>Neutral</button>
         <button onClick={() => handler(setBad)}>Bad</button>
       </div>
-      {all > 0 && <Statistics bad={bad} good={good} neutral={neutral} all={all} />}
+      <Statistics bad={bad} good={good} neutral={neutral} />
     </div>
   )
 }
-const Statistics = ({ good, neutral, bad, all }) => {
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
   return (
     <div>
       <h1>Statistics</h1>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {all}</p>
-      <p>Average {(good - bad) / all}</p>
-      <p>Positive {(good / all) * 100} %</p>
+      {all > 0 ? (
+        <>
+          <p>Good {good}</p>
+          <p>Neutral {neutral}</p>
+          <p>Bad {bad}</p>
+          <p>All {all}</p>
+          <p>Average {(good - bad) / all}</p>
+          <p>Positive {(good / all) * 100} %</p>
+        </>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   )
 }
