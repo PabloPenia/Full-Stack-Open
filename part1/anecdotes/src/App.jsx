1c373da6e7c40cde7e2d4577c7501aa4e06fd81d
Pablo Peña <pablopenia.dev@gmail.com>
2022-04-25 19:05:46 -0300

    1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
 const App = ({ anecdotes }) => {
+  const INITIAL = new Array(anecdotes.length).fill(0)
   const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(INITIAL)
   const getQuote = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+  const voteQuote = () => {
+    const newVote = [...vote]
+    newVote[selected] += 1
+    setVote([...newVote])
+  }
   return (
     <div>
       <h1>{anecdotes[selected]}</h1>
+      <p>
+        Has <strong>{vote[selected]}</strong> vote{vote[selected] > 1 && 's'}.
+      </p>
+      <button onClick={voteQuote}>Vote</button>
       <button onClick={getQuote}>Next Quote</button>
     </div>
   )
