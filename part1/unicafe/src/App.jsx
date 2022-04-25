437e1ee3d1622d6eafd90f59dd681c2a8446c8f1
Pablo Peña <pablopenia.dev@gmail.com>
2022-04-25 14:29:21 -0300

    1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,37 +3,40 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const handler = setter => setter(prev => prev + 1)
+  const all = good + neutral + bad
   return (
     <div>
       <h1>Give Feedback</h1>
       <div>
-        <button onClick={() => handler(setGood)}>Good</button>
-        <button onClick={() => handler(setNeutral)}>Neutral</button>
-        <button onClick={() => handler(setBad)}>Bad</button>
+        <Button text="Good" setter={setGood} />
+        <Button text="Neutral" setter={setNeutral} />
+        <Button text="Bad" setter={setBad} />
       </div>
-      <Statistics bad={bad} good={good} neutral={neutral} />
-    </div>
-  )
-}
-const Statistics = ({ good, neutral, bad }) => {
-  const all = good + neutral + bad
-  return (
-    <div>
       <h1>Statistics</h1>
       {all > 0 ? (
         <>
-          <p>Good {good}</p>
-          <p>Neutral {neutral}</p>
-          <p>Bad {bad}</p>
-          <p>All {all}</p>
-          <p>Average {(good - bad) / all}</p>
-          <p>Positive {(good / all) * 100} %</p>
+          <Statistics text="Good" value={good} />
+          <Statistics text="Neutral" value={neutral} />
+          <Statistics text="Bad" value={bad} />
+          <Statistics text="All" value={all} />
+          <Statistics text="Average" value={(good - bad) / all} />
+          <Statistics text="Positive" value={(good / all) * 100} />
         </>
       ) : (
-        <p>No feedback given</p>
+        <p>No Feedback Given</p>
       )}
     </div>
+  )
+}
+const Button = ({ text, setter }) => {
+  const handler = () => setter(prev => prev + 1)
+  return <button onClick={handler}>{text}</button>
+}
+const Statistics = ({ text, value }) => {
+  return (
+    <p>
+      {text} {value} {text === 'Positive' && '%'}
+    </p>
   )
 }
 export default App
