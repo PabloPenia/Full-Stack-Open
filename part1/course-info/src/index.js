58c0fa9747abd13bccfb481fdf6e7d232e687519
Pablo Peña <pablopenia.dev@gmail.com>
2022-04-24 22:41:06 -0300

    1.3

--- part1/course-info/src/index.js
+++ part1/course-info/src/index.js
@@ -3,28 +3,38 @@ import ReactDOM from 'react-dom/client'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  }
   return (
     <div>
-      <Header course={course} />
+      <Header title={course} />
       <Content>
-        <Part part={part1} number={exercises1} />
-        <Part part={part2} number={exercises2} />
-        <Part part={part3} number={exercises3} />
+        <Part part={part1} />
+        <Part part={part2} />
+        <Part part={part3} />
       </Content>
-      <Total ex={[exercises1, exercises2, exercises3]} />
+      <Total ex={[part1, part2, part3]} />
     </div>
   )
 }
-const Header = ({ course }) => <h1>{course}</h1>
+const Header = ({ title }) => <h1>{title}</h1>
 const Content = ({ children }) => children
-const Part = ({ part, number }) => <p>{`${part} ${number}`}</p>
-const Total = ({ ex }) => <p>Number of exercises {ex[0] + ex[1] + ex[2]}</p>
+const Part = ({ part }) => <p>{`${part.name} ${part.exercises}`}</p>
+const Total = ({ ex }) => {
+  const exTotal = ex.map(item => item.exercises).reduce((prev, curr) => prev + curr, 0)
+  return <p>Number of exercises {exTotal}</p>
+}
+
 // render application
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
