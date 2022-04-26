873dfd0409a90b882ab9d32bd60973dd329efee7
Pablo Peña <pablopenia.dev@gmail.com>
2022-04-26 09:08:10 -0300

    2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+function App() {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+  const addPerson = e => {
+    e.preventDefault()
+    const newPerson = { name: newName }
+    setPersons([...persons, newPerson])
+    setNewName('')
+  }
+  const handleChange = e => setNewName(e.target.value)
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>debug: {newName}</div>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  )
+}
+
+export default App
