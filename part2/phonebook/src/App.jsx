080e21a10a99e0e0e9c8f2e81a5a51b10f4bdb04
Pablo Peña <pablopenia.dev@gmail.com>
2022-04-27 11:09:15 -0300

    2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,33 +1,52 @@
 import { useState } from 'react'
-
+const DB = [
+  { name: 'Arto Hellas', phone: '040-123456' },
+  { name: 'Ada Lovelace', phone: '39-44-5323523' },
+  { name: 'Dan Abramov', phone: '12-43-234345' },
+  { name: 'Mary Poppendieck', phone: '39-23-6423122' },
+]
+const INITIAL = { name: '', phone: '' }
 function App() {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState(DB)
+  const [newContact, setNewContact] = useState(INITIAL)
+  const [keyword, setKeyword] = useState('')
+  // input handlers
   const addPerson = e => {
     e.preventDefault()
-    const exists = persons.some(person => person.name === newName)
-    if (exists) return alert(`${newName} is already added to phonebook.`)
+    const exists = persons.some(person => person.name === newContact.name)
+    if (exists) return alert(`${newContact.name} is already added to phonebook.`)
 
-    const newPerson = { name: newName }
-    setPersons([...persons, newPerson])
-    setNewName('')
+    setPersons([...persons, newContact])
+    setNewContact(INITIAL)
   }
-  const handleChange = e => setNewName(e.target.value)
+  const handleName = e => setNewContact({ ...newContact, name: e.target.value })
+  const handlePhone = e => setNewContact({ ...newContact, phone: e.target.value })
+  const handleKeyword = e => setKeyword(e.target.value)
+  // other functions
+  const filtered = (arr, key) => arr.filter(person => (key !== '' ? person.name.toLowerCase().includes(key.toLowerCase()) : true))
   return (
     <div>
-      <h2>Phonebook</h2>
-      <div>debug: {newName}</div>
+      <h1>Phonebook</h1>
+      <div>
+        filter shown with <input value={keyword} onChange={handleKeyword} />
+      </div>
+      <h2>Add New</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input value={newContact.name} onChange={handleName} />
+        </div>
+        <div>
+          number: <input value={newContact.phone} onChange={handlePhone} />
         </div>
         <div>
           <button>add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {persons.map(person => (
-        <p key={person.name}>{person.name}</p>
+      <h2>Contacts</h2>
+      {filtered(persons, keyword).map(person => (
+        <p key={person.name}>
+          {person.name} {person.phone}
+        </p>
       ))}
     </div>
   )
